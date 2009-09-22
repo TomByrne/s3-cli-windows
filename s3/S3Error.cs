c46262861a8a042d461ef174b326ab4530958343
SND\maxc_cp <SND\maxc_cp@ffd33b8c-2492-42e0-bdc5-587b920b7d6d>
2009-09-22 06:17:00 +0000

    Added file missing from previous commit (S3Error.cs)
    git-svn-id: https://s3.svn.codeplex.com/svn@30796 ffd33b8c-2492-42e0-bdc5-587b920b7d6d

---
+++ s3/S3Error.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace s3
+{
+    [XmlType("Error")]
+    public class S3Error
+    {
+        [XmlElement]
+        public string Code;
+
+        [XmlElement]
+        public string Message;
+    }
+}
