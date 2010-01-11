10a9a29784e0d161da7e3aaa0819583a5c3c0734
SND\maxc_cp <SND\maxc_cp@ffd33b8c-2492-42e0-bdc5-587b920b7d6d>
2010-01-11 15:07:38 +0000

    Updated version number to 1.5
    git-svn-id: https://s3.svn.codeplex.com/svn@36755 ffd33b8c-2492-42e0-bdc5-587b920b7d6d

--- s3/Properties/AssemblyInfo.cs
+++ s3/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("http://s3.codeplex.com")]
-[assembly: AssemblyCopyright("Copyright ©  2008")]
+[assembly: AssemblyCopyright("Copyright ©  2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.0.0")]
-[assembly: AssemblyFileVersion("1.4.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
